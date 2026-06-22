eabc4d130b67be9cb3777fc2ee5eba2871b67636
Samir Mideros <seimirmideros@gmail.com>
2026-06-22 08:10:01 -0500

    feat(jwt): implementar firmado asimetrico rs256 y middleware de validacion autonoma public-key

--- src/instrument.js
+++ src/instrument.js
@@ -1,13 +1,12 @@
 const Sentry = require('@sentry/node');
-require('dotenv').config();
-
-const dsn = process.env.SENTRY_DSN;
 
 Sentry.init({
-  dsn,
-  tracesSampleRate: 1.0,
-  environment: process.env.NODE_ENV || 'development',
-  release: process.env.npm_package_version || 'unknown'
+  dsn: 'https://78c8bfc732db0aaedee099da45920bff@o4511580770861056.ingest.us.sentry.io/4511580783771648',
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below.
+    // userInfo: false,
+    // httpBodies: [],
+  },
 });
 
 module.exports = Sentry;
